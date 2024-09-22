f2e002073862d915c526c43371ce7baac936c0dc
benji77430 <85637652+benji77430@users.noreply.github.com>
2024-09-22 21:05:08 +0200

    Update system.py

--- system.py
+++ system.py
@@ -5,9 +5,6 @@ system = platform.system()
 print(system)
 if system == "Windows":
   os.system('curl -fsSL -o SpotAFan.py https://raw.githubusercontent.com/benji77430/SpotAFan/refs/heads/main/SpotAFan.py && python SpotAFan.py ')
-elif system == "Darwin":
-    print("MacOS detected")
-
 elif system == "Linux":
     print("LINUX detected")
     os.system('curl -fsSL -o SpotAFan.py https://raw.githubusercontent.com/benji77430/SpotAFan/linux/SpotAFan.py && python SpotAFan.py')
