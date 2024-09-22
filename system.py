52b75230d32fbbc58cee74d5155ad9bacb211c88
benji77430 <85637652+benji77430@users.noreply.github.com>
2024-09-22 21:04:46 +0200

    Update system.py

--- system.py
+++ system.py
@@ -4,13 +4,13 @@ import os
 system = platform.system()
 print(system)
 if system == "Windows":
-  os.system('curl -fsSl https://raw.githubusercontent.com/benji77430/SpotAFan/main/SpotAFan.py | python')
+  os.system('curl -fsSL -o SpotAFan.py https://raw.githubusercontent.com/benji77430/SpotAFan/refs/heads/main/SpotAFan.py && python SpotAFan.py ')
 elif system == "Darwin":
     print("MacOS detected")
 
 elif system == "Linux":
     print("LINUX detected")
-    os.system('curl -fsSL https://raw.githubusercontent.com/benji77430/SpotAFan/linux/SpotAFan.py | python')
+    os.system('curl -fsSL -o SpotAFan.py https://raw.githubusercontent.com/benji77430/SpotAFan/linux/SpotAFan.py && python SpotAFan.py')
   
 else:
     raise OSError(f"Unsupported operating system: {system}")
